7ec30005133988af8c7132a8a1def5322f0865ba
zhuravliova <sombrrre@gmail.com>
2020-01-09 22:23:15 +0200

    add correct test name

--- test_items.py
+++ test_items.py
@@ -3,8 +3,8 @@ import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_items(browser):
+def test_site_accepts_language(browser):
 	browser.get(link)
-	time.sleep(15)
+	time.sleep(30)
 	button = find_element_by_css_selector(".btn-add-to-basket")
 	assert button
